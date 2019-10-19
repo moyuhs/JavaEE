bd0634435505872a6cac52e58163ebf3f11a34ed
Rainbow Cat <2056423011@qq.com>
2019-10-19 11:06:00 +0800

    代码优化

--- src/main/java/com/java/jdbc/Demo01Jdbc.java
+++ src/main/java/com/java/jdbc/Demo01Jdbc.java
@@ -15,13 +15,14 @@ public class Demo01Jdbc {
             //1. 注册驱动
             Class.forName( "com.mysql.jdbc.Driver" );
             //2. 定义sql
-            String sql = "insert into Student values(null,'琳琳',18)";
+            String insertSql = "insert into Student values(null,'琳琳',18)";
+            String updateSql = "update Student set name = '椎名真白' where id = 4  ";
             //3.获取Connection对象
             conn = DriverManager.getConnection( "jdbc:mysql://47.97.185.117:3306/study?useUnicode=true&characterEncoding=utf-8&useSSL=false", "admin", "admin" );
             //4.获取执行sql的对象 Statement
             stmt = conn.createStatement();
             //5.执行sql,返回受影响的行数
-            int count = stmt.executeUpdate( sql );
+            int count = stmt.executeUpdate( updateSql );
             //6.处理结果
             out.println( count );
             if (count > 0) {
@@ -29,7 +30,6 @@ public class Demo01Jdbc {
             } else {
                 out.println( "添加失败！" );
             }
-
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         } finally {
